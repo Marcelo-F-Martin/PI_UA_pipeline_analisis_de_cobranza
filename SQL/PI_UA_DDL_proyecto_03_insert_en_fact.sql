597731a149a1140768ee0a105790cb17ece0bb30
Marcelo-F-Martin <marcemartin8@hotmail.com>
2026-03-20 15:51:55 -0300

    Add files via upload

--- SQL/PI_UA_DDL_proyecto_03_insert_en_fact.sql
+++ SQL/PI_UA_DDL_proyecto_03_insert_en_fact.sql
@@ -1,5 +1,10 @@
-insert into cobranzas_capa_uno.fact_cobranzas (	
-	periodo, 
+/* INGESTA EN TABLA DE HECHOS
+	Mueve datos de tabla temporal a tabla principal
+*/
+DELETE FROM cobranzas_capa_uno.fact_cobranzas 
+WHERE periodo IN (SELECT DISTINCT periodo FROM cobranzas_capa_uno.temp_cobranzas);
+INSERT INTO cobranzas_capa_uno.fact_cobranzas 
+	(periodo, 
 	asesor, 
 	fecha_operacion, 
 	contrato, 
@@ -8,18 +13,15 @@ insert into cobranzas_capa_uno.fact_cobranzas (
 	porcentaje_comision, 
 	forma_pago, 
 	numero_recibo)
-(select 
-	periodo,
-	asesor,
-	fecha_operacion,
-	contrato,
-	comision,
-	valor_neto,
-	porcentaje_comision,
-	forma_pago,
-	numero_recibo
-from cobranzas_capa_uno.temp_cobranzas
-    );
-
-select * from cobranzas_capa_uno.temp_cobranzas limit 10;
-select count(*) from cobranzas_capa_uno.temp_cobranzas;
+	(SELECT 
+		periodo,
+		asesor,
+		fecha_operacion,
+		contrato,
+		comision,
+		valor_neto,
+		porcentaje_comision,
+		forma_pago,
+		numero_recibo
+	FROM cobranzas_capa_uno.temp_cobranzas
+	);
