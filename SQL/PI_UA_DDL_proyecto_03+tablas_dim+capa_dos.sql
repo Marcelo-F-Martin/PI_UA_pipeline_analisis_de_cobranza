2b69fcaecf359bb9e627dcee566b15e56299c18f
Marcelo-F-Martin <marcemartin8@hotmail.com>
2026-03-24 22:14:30 -0300

    Add files via upload

--- SQL/PI_UA_DDL_proyecto_03+tablas_dim+capa_dos.sql
+++ SQL/PI_UA_DDL_proyecto_03+tablas_dim+capa_dos.sql
@@ -12,6 +12,63 @@ CREATE DATABASE cobranzas_capa_uno;
 DROP DATABASE IF EXISTS cobranzas_capa_dos;
 CREATE DATABASE cobranzas_capa_dos;
 
+
+/*EJEMPLO tabla calendario
+USE db_seguros_gold;
+
+CREATE TABLE IF NOT EXISTS dim_calendario (
+    fecha DATE PRIMARY KEY,
+    anio INT NOT NULL,
+    mes INT NOT NULL,
+    nombre_mes VARCHAR(15) NOT NULL,
+    mes_corto CHAR(3) NOT NULL,
+    dia INT NOT NULL,
+    dia_semana INT NOT NULL, -- 1 para Lunes, 7 para Domingo
+    nombre_dia VARCHAR(15) NOT NULL,
+    trimestre INT NOT NULL,
+    es_fin_de_semana BOOLEAN NOT NULL,
+    -- Útil para ordenar meses cronológicamente y no alfabéticamente
+    mes_id INT NOT NULL -- Ejemplo: 202603
+);
+
+DELIMITER //
+
+CREATE PROCEDURE llenar_calendario(IN fecha_inicio DATE, IN fecha_fin DATE)
+BEGIN
+    DECLARE fecha_actual DATE;
+    SET fecha_actual = fecha_inicio;
+    
+    WHILE fecha_actual <= fecha_fin DO
+        INSERT INTO dim_calendario (
+            fecha, anio, mes, nombre_mes, mes_corto, dia, 
+            dia_semana, nombre_dia, trimestre, es_fin_de_semana, mes_id
+        ) VALUES (
+            fecha_actual,
+            YEAR(fecha_actual),
+            MONTH(fecha_actual),
+            MONTHNAME(fecha_actual),
+            LEFT(MONTHNAME(fecha_actual), 3),
+            DAY(fecha_actual),
+            WEEKDAY(fecha_actual) + 1,
+            DAYNAME(fecha_actual),
+            QUARTER(fecha_actual),
+            IF(WEEKDAY(fecha_actual) IN (5, 6), 1, 0),
+            (YEAR(fecha_actual) * 100) + MONTH(fecha_actual)
+        );
+        SET fecha_actual = DATE_ADD(fecha_actual, INTERVAL 1 DAY);
+    END WHILE;
+END //
+
+DELIMITER ;
+
+-- Ejecutar para generar 10 años de fechas
+CALL llenar_calendario('2020-01-01', '2030-12-31');
+
+
+
+*/
+
+
 /* 1.2 - TABLAS DE HECHO
 	1.2.1 - Crea tabla de hechos para estacionamiento de datos ingestados, previo a carga en tabla principal fact_cobranzas.
 			Esta tabla se sobreescribe con cada ingesta (truncate + insert) desde script python.
