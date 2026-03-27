95ff422bb5b16d4eee5c8c3c5914237b726a4672
Marcelo-F-Martin <marcemartin8@hotmail.com>
2026-03-27 12:55:25 -0300

    Update PI_UA_DDL_proyecto_03+tablas_dim+capa_dos.sql

--- SQL/PI_UA_DDL_proyecto_03+tablas_dim+capa_dos.sql
+++ SQL/PI_UA_DDL_proyecto_03+tablas_dim+capa_dos.sql
@@ -7,7 +7,6 @@
         Capa dos: Consulta de capa uno y almacena Vistas para servicio de BI.
 */
 
-DELIMITER ;
 DROP DATABASE IF EXISTS cobranzas_capa_uno;
 CREATE DATABASE cobranzas_capa_uno;
 DROP DATABASE IF EXISTS cobranzas_capa_dos;
@@ -71,39 +70,6 @@ CREATE TABLE IF NOT EXISTS cobranzas_capa_uno.dim_calendario (
     es_fin_de_semana BOOLEAN NOT NULL,
     mes_id INT NOT NULL -- Ejemplo: 202603
 );
-
-DELIMITER //
-DROP PROCEDURE IF EXISTS cobranzas_capa_uno.llenar_calendario;
-CREATE PROCEDURE cobranzas_capa_uno.llenar_calendario(IN fecha_inicio DATE, IN fecha_fin DATE)
-BEGIN
-    DECLARE fecha_actual DATE;
-    SET fecha_actual = fecha_inicio;
-    
-    WHILE fecha_actual <= fecha_fin DO
-        INSERT INTO cobranzas_capa_uno.dim_calendario (
-            fecha, anio, mes, nombre_mes, mes_corto, dia, 
-            dia_semana, nombre_dia, trimestre, es_fin_de_semana, mes_id
-        ) VALUES (
-            fecha_actual,
-            YEAR(fecha_actual),
-            MONTH(fecha_actual),
-            MONTHNAME(fecha_actual),
-            LEFT(MONTHNAME(fecha_actual), 3),
-            DAY(fecha_actual),
-            WEEKDAY(fecha_actual) + 1,
-            DAYNAME(fecha_actual),
-            QUARTER(fecha_actual),
-            IF(WEEKDAY(fecha_actual) IN (5, 6), 1, 0),
-            (YEAR(fecha_actual) * 100) + MONTH(fecha_actual)
-        );
-        SET fecha_actual = DATE_ADD(fecha_actual, INTERVAL 1 DAY);
-    END WHILE;
-END //
-
-DELIMITER ;
--- Ejecutar para generar 10 años de fechas
-CALL cobranzas_capa_uno.llenar_calendario('2020-01-01', '2030-12-31');
-
 
 DROP TABLE IF EXISTS cobranzas_capa_uno.dim_provincia;
 CREATE TABLE cobranzas_capa_uno.dim_provincia (
