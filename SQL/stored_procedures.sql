fe70d42579eab348e6332c61fc6ede558a405a7a
Marcelo-F-Martin <marcemartin8@hotmail.com>
2026-03-27 13:29:51 -0300

    Create stored_procedures.sql

---
+++ SQL/stored_procedures.sql
@@ -0,0 +1,28 @@
+CREATE PROCEDURE cobranzas_capa_uno.llenar_calendario(IN fecha_inicio DATE, IN fecha_fin DATE)
+BEGIN
+    DECLARE fecha_actual DATE;
+    SET fecha_actual = fecha_inicio;
+    
+    WHILE fecha_actual <= fecha_fin DO
+        INSERT INTO cobranzas_capa_uno.dim_calendario (
+            fecha, anio, mes, nombre_mes, mes_corto, dia, 
+            dia_semana, nombre_dia, trimestre, es_fin_de_semana, mes_id
+        ) VALUES (
+            fecha_actual,
+            YEAR(fecha_actual),
+            MONTH(fecha_actual),
+            MONTHNAME(fecha_actual),
+            LEFT(MONTHNAME(fecha_actual), 3),
+            DAY(fecha_actual),
+            WEEKDAY(fecha_actual) + 1,
+            DAYNAME(fecha_actual),
+            QUARTER(fecha_actual),
+            IF(WEEKDAY(fecha_actual) IN (5, 6), 1, 0),
+            (YEAR(fecha_actual) * 100) + MONTH(fecha_actual)
+        );
+        SET fecha_actual = DATE_ADD(fecha_actual, INTERVAL 1 DAY);
+    END WHILE;
+END;
+
+-- Ejecutar para generar 10 años de fechas
+CALL cobranzas_capa_uno.llenar_calendario('2020-01-01', '2030-12-31');
